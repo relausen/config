037e1370bcd5cd3e8aceed28d589021264be54b0
Rune Lausen <rune@lausennet.dk>
2023-11-08 18:38:04 +0100

    Add some useful Neovim Telescope extensions

--- .config/nvim/lua/rel/plugins/telescope.lua
+++ .config/nvim/lua/rel/plugins/telescope.lua
@@ -5,6 +5,8 @@ return {
         'nvim-lua/plenary.nvim',
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
         "nvim-tree/nvim-web-devicons",
+        "nvim-telescope/telescope-file-browser.nvim",
+        "smilovanovic/telescope-search-dir-picker.nvim",
     },
     config = function()
         local telescope = require("telescope")
@@ -19,15 +21,18 @@ return {
                         ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
                     }
                 }
-            }
+            },
         })
 
-        telescope.load_extension("fzf") 
+        telescope.load_extension("fzf")
+        telescope.load_extension("search_dir_picker")
+        telescope.load_extension("file_browser")
 
         local keymap = vim.keymap
+        keymap.set("n", "<leader>fb", "<cmd>Telescope file_browser<cr>", { desc = "Browse files" })
+        keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
         keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
         keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
         keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-        keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
     end
 }
