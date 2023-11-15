7b5502a5459f685ac1ffa7195eaf0d47f4af7a84
Rune Lausen <rune.lausen@lego.com>
2023-11-15 12:56:36 +0100

    Add keymaps for Python DAP

--- .config/nvim/after/ftplugin/python.lua
+++ .config/nvim/after/ftplugin/python.lua
@@ -1,2 +1,16 @@
 -- vim.g.pyindent_open_paren = 'shiftwidth()'
 -- vim.g.pyindent_continue = 'shiftwidth()'
+
+local keymap = vim.keymap
+local dap_python = require('dap-python')
+
+local opts = { noremap = true, silent = true }
+
+opts.desc = "Run test method/function"
+keymap.set("n", "<leader>dm", dap_python.test_method, opts)
+
+opts.desc = "Run test class"
+keymap.set("n", "<leader>dc", dap_python.test_class, opts)
+
+opts.desc = "Debug selection"
+keymap.set("n", "<leader>ds", dap_python.debug_selection, opts)
