9cf0cb862c92228fc5bc1f75234e7a9c002d49a7
Rune Lausen <rune.lausen@lego.com>
2023-11-18 13:17:30 +0100

    Fix indentation

--- .config/nvim/lua/rel/plugins/venv-selector.lua
+++ .config/nvim/lua/rel/plugins/venv-selector.lua
@@ -1,13 +1,13 @@
 return {
-	"linux-cultist/venv-selector.nvim",
-	-- dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
-	dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", },
-    event = "VeryLazy",
-    opts = {
-        parents = 1,
-    },
-    keys = {
-        { "<leader>ves", "<cmd>VenvSelect<cr>" },
-        { "<leader>vec", "<cmd>VenvSelectCached<cr>" },
-    }
+  "linux-cultist/venv-selector.nvim",
+  dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
+  -- dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", },
+  event = "VeryLazy",
+  opts = {
+    parents = 1,
+  },
+  keys = {
+    { "<leader>ves", "<cmd>VenvSelect<cr>" },
+    { "<leader>vec", "<cmd>VenvSelectCached<cr>" },
+  }
 }
