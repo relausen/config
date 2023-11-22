07c3b0e14332c3f5db421f0c8de23855137999e7
Rune Lausen <rune.lausen@lego.com>
2023-11-22 20:43:46 +0100

    Fix indentation

--- .config/nvim/lua/rel/plugins/telescope.lua
+++ .config/nvim/lua/rel/plugins/telescope.lua
@@ -1,41 +1,41 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.4',
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-        "nvim-telescope/telescope-dap.nvim",
-        "nvim-tree/nvim-web-devicons",
-        "nvim-telescope/telescope-file-browser.nvim",
-        "smilovanovic/telescope-search-dir-picker.nvim",
-    },
-    config = function()
-        local telescope = require("telescope")
-        local actions = require("telescope.actions")
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.4',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    "nvim-telescope/telescope-dap.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "nvim-telescope/telescope-file-browser.nvim",
+    "smilovanovic/telescope-search-dir-picker.nvim",
+  },
+  config = function()
+    local telescope = require("telescope")
+    local actions = require("telescope.actions")
 
-        telescope.setup({
-            defaults = {
-                mappings = {
-                    i = {
-                        ["<C-k>"] = actions.move_selection_previous,
-                        ["<C-j>"] = actions.move_selection_next,
-                        ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-                    }
-                }
-            },
-        })
+    telescope.setup({
+      defaults = {
+        mappings = {
+          i = {
+            ["<C-k>"] = actions.move_selection_previous,
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+          }
+        }
+      },
+    })
 
-        telescope.load_extension("fzf")
-        telescope.load_extension("search_dir_picker")
-        telescope.load_extension("file_browser")
-        telescope.load_extension("dap")
+    telescope.load_extension("fzf")
+    telescope.load_extension("search_dir_picker")
+    telescope.load_extension("file_browser")
+    telescope.load_extension("dap")
 
-        local keymap = vim.keymap
-        keymap.set("n", "<leader>fb", "<cmd>Telescope file_browser<cr>", { desc = "Browse files" })
-        keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-        keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-        keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { desc = "Fuzzy find help" })
-        keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-        keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-    end
+    local keymap = vim.keymap
+    keymap.set("n", "<leader>fb", "<cmd>Telescope file_browser<cr>", { desc = "Browse files" })
+    keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+    keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+    keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { desc = "Fuzzy find help" })
+    keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+    keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+  end
 }
