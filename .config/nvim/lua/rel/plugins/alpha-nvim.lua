66bbec90c95811cf77943c3e26aea10f059791f4
Rune Lausen <rune@lausennet.dk>
2023-12-20 20:19:43 +0100

    Simplify Alpha menu

--- .config/nvim/lua/rel/plugins/alpha-nvim.lua
+++ .config/nvim/lua/rel/plugins/alpha-nvim.lua
@@ -19,11 +19,11 @@ return {
 
     -- Set menu
     dashboard.section.buttons.val = {
-      dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
-      dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
-      dashboard.button("SPC ff", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
-      dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
-      dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
+      dashboard.button("e", "  > New File", "<cmd>enew<CR>"),
+      -- dashboard.button("p", "  > Projects", "<cmd>Telescope <CR>"),
+    --   dashboard.button("SPC ff", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
+    --   -- dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
+    --   dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
       dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
     }
 
