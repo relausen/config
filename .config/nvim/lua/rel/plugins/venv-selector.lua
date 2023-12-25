fd71668758a3fd117fd12cf3da7b585e8ec675e5
Rune Lausen <rune@lausennet.dk>
2023-12-25 17:15:55 +0100

    Improve formatting

--- .config/nvim/lua/rel/plugins/venv-selector.lua
+++ .config/nvim/lua/rel/plugins/venv-selector.lua
@@ -1,7 +1,10 @@
 return {
   "linux-cultist/venv-selector.nvim",
-  dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
-  -- dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", },
+  dependencies = {
+    "neovim/nvim-lspconfig",
+    "nvim-telescope/telescope.nvim",
+    -- "mfussenegger/nvim-dap-python"
+  },
   event = "VeryLazy",
   opts = {
     parents = 1,
