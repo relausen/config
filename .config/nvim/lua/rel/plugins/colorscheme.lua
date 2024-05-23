e78dbe9e7158d414575619a38f52e3474363b189
Rune Lausen <rune.lausen@lego.com>
2024-05-23 09:27:07 +0200

    Make linenumbers easier to read in Neovim

--- .config/nvim/lua/rel/plugins/colorscheme.lua
+++ .config/nvim/lua/rel/plugins/colorscheme.lua
@@ -1,15 +1,18 @@
 return {
-	-- "EdenEast/nightfox.nvim",
   "folke/tokyonight.nvim",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		-- vim.cmd("colorscheme nightfox")
+  lazy = false,
+  priority = 1000,
+  config = function()
     require("tokyonight").setup({
       style = "night",
       dim_inactive = true,
       lualine_bold = true,
+      hide_inactive_statusline = true,
+      on_highlights = function(highlights, colors)
+        highlights.LineNrAbove = { fg = colors.fg_dark }
+        highlights.LineNrBelow = { fg = colors.fg_dark }
+      end,
     })
-		vim.cmd("colorscheme tokyonight")
-	end,
+    vim.cmd("colorscheme tokyonight")
+  end,
 }
