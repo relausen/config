07c4ee4410dc57a6377dc64dba646def6ccf3d2c
Rune Lausen <rune@lausennet.dk>
2024-06-14 19:52:38 +0200

    Neovim: Don't lazy load nvim-notify

--- .config/nvim/lua/rel/plugins/nvim-notify.lua
+++ .config/nvim/lua/rel/plugins/nvim-notify.lua
@@ -1,9 +1,8 @@
 return {
-    "rcarriga/nvim-notify",
-    lazy = false,
-    event = { "BufReadPre", "BufNewFile" },
-    opts = {
-        level = vim.log.levels.INFO,
-        timeout = 1000,
-    },
+  "rcarriga/nvim-notify",
+  event = { "BufReadPre", "BufNewFile" },
+  opts = {
+    level = vim.log.levels.INFO,
+    timeout = 1000,
+  },
 }
