5d2ad43a1da8dcc479b71667af8d831542593fc5
Rune Lausen <rune@lausennet.dk>
2024-06-17 07:37:41 +0200

    Neovim: Use Ruff formatter properly

--- .config/nvim/lua/rel/plugins/formatting.lua
+++ .config/nvim/lua/rel/plugins/formatting.lua
@@ -1,26 +1,27 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local conform = require("conform")
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local conform = require("conform")
 
-		conform.setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				python = { "ruff" },
-				rust = { "rustfmt" },
-			},
-			format_on_save = {
-				timeout_ms = 500,
-				lsp_fallback = true,
-			},
-		})
+    conform.setup({
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "ruff_organize_imports", "ruff_format" },
+        rust = { "rustfmt" },
+        terraform = { "terraform_fmt" }, -- Use "hcl" instead?
+      },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+    })
 
-		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-			conform.format({
-				timeout_ms = 500,
-				lsp_fallback = true,
-			})
-		end, { desc = "Format file or range" })
-	end,
+    vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+      conform.format({
+        timeout_ms = 500,
+        lsp_fallback = true,
+      })
+    end, { desc = "Format file or range" })
+  end,
 }
