57614119b6622a13e8e8611830f07d93cb19a8fc
Rune Lausen <rune@lausennet.dk>
2024-06-30 12:12:10 +0200

    Neovim Rest: Adapt key bindings for lates version

--- .config/nvim/after/ftplugin/http.lua
+++
@@ -1,5 +0,0 @@
-local map = vim.keymap
-
-map.set("n", "<leader>xr", "<Plug>RestNvim", { silent = true, buffer = true })
-map.set("n", "<leader>xp", "<Plug>RestNvimPreview", { silent = true, buffer = true })
-map.set("n", "<leader>xl", "<Plug>RestNvimLast", { silent = true, buffer = true })
--- .config/nvim/lua/rel/plugins/rest-nvim.lua
+++ .config/nvim/lua/rel/plugins/rest-nvim.lua
@@ -3,6 +3,11 @@ return {
   "rest-nvim/rest.nvim",
   ft = "http",
   config = function()
-    require("rest-nvim").setup()
+    require("rest-nvim").setup({
+      keybinds = {
+        { "<leader>xr", "<cmd>Rest run<cr>", "Run request under cursor" },
+        { "<leader>xl", "<cmd>Rest run last<cr>", "Re-run latest request" },
+      },
+    })
   end,
 }
