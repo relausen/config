28f5341152934299219459052d0198492efd464f
Rune Lausen <rune@lausennet.dk>
2024-07-06 12:27:45 +0200

    Neovim: Enable WezTerm integration in Zen mode

--- .config/nvim/lua/rel/plugins/zen-mode.lua
+++ .config/nvim/lua/rel/plugins/zen-mode.lua
@@ -2,10 +2,17 @@ return {
   "folke/zen-mode.nvim",
   opts = {
     plugins = {
+      gitsigns = {
+        enabled = true,
+      },
       kitty = {
         enabled = true,
         font = "+3",
       },
-    }
-  }
+      wezterm = {
+        enabled = true,
+        font = "+3",
+      },
+    },
+  },
 }
