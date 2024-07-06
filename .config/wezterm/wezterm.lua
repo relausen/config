0f9f964d717c80296c55f262fbd0d774ee3c4fbe
Rune Lausen <rune@lausennet.dk>
2024-07-06 12:29:57 +0200

    WezTerm: Enable Neovim Zen mode support

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,30 +1,30 @@
 local font_size <const> = 13
 
-local wezterm = require 'wezterm'
-local key_bindings = require 'key-bindings'
-local hyperlinks = require('hyperlinks')
+local wezterm = require("wezterm")
+local key_bindings = require("key-bindings")
+local hyperlinks = require("hyperlinks")
 
 local mux = wezterm.mux
 local config = wezterm.config_builder()
 
 config.audible_bell = "Disabled"
-config.color_scheme = 'Tokyo Night'
+config.color_scheme = "Tokyo Night"
 config.cursor_thickness = 2
-config.default_cursor_style = 'SteadyBar'
-config.font = wezterm.font('JetBrainsMono Nerd Font Mono', { weight = 'Medium' })
+config.default_cursor_style = "SteadyBar"
+config.font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = "Medium" })
 config.font_size = font_size
 config.hyperlink_rules = hyperlinks.hyperlink_rules
 config.inactive_pane_hsb = {
   saturation = 0.24,
-  brightness = 0.5
+  brightness = 0.5,
 }
 config.keys = key_bindings.keys
 config.key_tables = key_bindings.key_tables
-config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 config.tab_bar_at_bottom = false
-config.window_decorations = 'RESIZE'
+config.window_decorations = "RESIZE"
 config.window_frame = {
-  font_size = font_size
+  font_size = font_size,
 }
 config.window_padding = {
   left = "0.5cell",
@@ -34,7 +34,7 @@ config.window_padding = {
 }
 
 local function basename(s)
-  return string.gsub(s, '(.*[/\\])(.*)', '%2')
+  return string.gsub(s, "(.*[/\\])(.*)", "%2")
 end
 
 local function tab_title(tab_info)
@@ -53,32 +53,50 @@ wezterm.on("gui-startup", function(cmd)
   window:gui_window():maximize()
 end)
 
-wezterm.on(
-  'format-tab-title',
-  function(tab, tabs, panes, config, hover, max_width)
-    local home_dir = os.getenv('HOME')
-    local pane = tab.active_pane
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+  local home_dir = os.getenv("HOME")
+  local pane = tab.active_pane
 
-    local title = tab_title(tab)
+  local title = tab_title(tab)
 
-    local current_dir = pane.current_working_dir.file_path:sub(1, -2):gsub(home_dir, '~')
-    return {
-      {
-        Text = current_dir .. ' - ' .. title .. ' '
-      },
-    }
-  end
-)
+  local current_dir = pane.current_working_dir.file_path:sub(1, -2):gsub(home_dir, "~")
+  return {
+    {
+      Text = current_dir .. " - " .. title .. " ",
+    },
+  }
+end)
 
-wezterm.on('update-right-status', function(window, pane)
+wezterm.on("update-right-status", function(window, pane)
   local info = pane:get_foreground_process_info()
   if info then
-    window:set_right_status(
-      table.concat(info.argv, ' ')
-    )
+    window:set_right_status(table.concat(info.argv, " "))
   else
-    window:set_right_status ''
+    window:set_right_status("")
   end
+end)
+
+wezterm.on("user-var-changed", function(window, pane, name, value)
+  local overrides = window:get_config_overrides() or {}
+  if name == "ZEN_MODE" then
+    local incremental = value:find("+")
+    local number_value = tonumber(value)
+    if incremental ~= nil then
+      while number_value > 0 do
+        window:perform_action(wezterm.action.IncreaseFontSize, pane)
+        number_value = number_value - 1
+      end
+      overrides.enable_tab_bar = false
+    elseif number_value < 0 then
+      window:perform_action(wezterm.action.ResetFontSize, pane)
+      overrides.font_size = nil
+      overrides.enable_tab_bar = true
+    else
+      overrides.font_size = number_value
+      overrides.enable_tab_bar = false
+    end
+  end
+  window:set_config_overrides(overrides)
 end)
 
 return config
