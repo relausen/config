2d3884a5268f988ef41273d4c05b860cbaa9d6f0
Rune Lausen <rune@lausennet.dk>
2024-07-06 11:47:54 +0200

    Neovim: A bit of plugin cleanup

--- .config/nvim/lua/rel/plugins/neovim-session-manager.lua
+++ .config/nvim/lua/rel/plugins/neovim-session-manager.lua
@@ -1,14 +1,14 @@
 return {
-  "Shatur/neovim-session-manager",
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-  },
-  config = function ()
-    local session_manager = require('session_manager')
-    local config = require('session_manager.config')
+  -- "Shatur/neovim-session-manager",
+  -- dependencies = {
+  --   'nvim-lua/plenary.nvim',
+  -- },
+  -- config = function ()
+  --   local session_manager = require('session_manager')
+  --   local config = require('session_manager.config')
 
-    session_manager.setup({
-      autoload_mode = config.AutoloadMode.Disabled,
-    })
-  end,
+  --   session_manager.setup({
+  --     autoload_mode = config.AutoloadMode.Disabled,
+  --   })
+  -- end,
 }
