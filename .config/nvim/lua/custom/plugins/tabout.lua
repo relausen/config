8a4bed4525c0d7e0421ba0e0fae68dac7e665452
Rune Lausen <rune.lausen@lego.com>
2024-07-23 16:37:10 +0200

    fix(neovim): Disable tabout. It's in my way.

--- .config/nvim/lua/custom/plugins/tabout.lua
+++ .config/nvim/lua/custom/plugins/tabout.lua
@@ -1,11 +1,11 @@
 return {
   "abecodes/tabout.nvim",
+  enabled = false,
   dependencies = {
     "hrsh7th/nvim-cmp",
     "nvim-treesitter/nvim-treesitter",
   },
-  config = function ()
-    require('tabout').setup {
-    }
-  end
+  config = function()
+    require("tabout").setup({})
+  end,
 }
