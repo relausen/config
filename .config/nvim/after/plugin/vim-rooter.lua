53cac38a43d8a1437baf82a8f6f1299bf36dd6c8
Rune Lausen <rune@lausennet.dk>
2024-09-06 07:51:06 +0200

    feat(neovim): Add Neorg plugin

--- .config/nvim/after/plugin/vim-rooter.lua
+++ .config/nvim/after/plugin/vim-rooter.lua
@@ -1,4 +1,4 @@
 vim.cmd([[
-let g:rooter_patterns = ['.git', '_darcs', '.hg', '.bzr', '.svn', 'Makefile', 'package.json']
+let g:rooter_patterns = ['.git', '_darcs', '.hg', '.bzr', '.svn', 'Makefile', 'package.json', 'index.norg']
 let g:rooter_cd_cmd = 'lcd'
 ]])
