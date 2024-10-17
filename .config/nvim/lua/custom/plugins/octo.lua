cd1cfcc01ae334641e62f6efda85597f584fae14
Rune Lausen <rune@lausennet.dk>
2024-10-17 21:12:29 +0200

    feat(neovim): Add lazygit and octo plugins

---
+++ .config/nvim/lua/custom/plugins/octo.lua
@@ -0,0 +1,12 @@
+return {
+  "pwntester/octo.nvim",
+  requires = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+    -- OR 'ibhagwan/fzf-lua',
+    "nvim-tree/nvim-web-devicons",
+  },
+  opts = {
+    default_to_projects_v2 = true,
+  },
+}
