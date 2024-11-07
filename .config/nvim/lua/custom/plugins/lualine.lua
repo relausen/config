15edc27f24dfb1901b0431e1c7c244548a0b0efe
Rune Lausen <rune@lausennet.dk>
2024-11-07 17:19:05 +0100

    chore(neovim): Formatting

--- .config/nvim/lua/custom/plugins/lualine.lua
+++ .config/nvim/lua/custom/plugins/lualine.lua
@@ -1,10 +1,9 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {
-        options = {
-            -- theme = 'auto',
-            theme = 'tokyonight',
-        }
-    }
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+    options = {
+      theme = "tokyonight",
+    },
+  },
 }
