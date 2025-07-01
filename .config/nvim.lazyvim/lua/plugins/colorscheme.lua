2b6f61e549cf8e037615b7ed890214ca79ee6906
Rune Lausen <rune.lausen@lego.com>
2025-07-01 16:46:10 +0200

    feat(neovim): Improve readability of line numbers

--- .config/nvim.lazyvim/lua/plugins/colorscheme.lua
+++ .config/nvim.lazyvim/lua/plugins/colorscheme.lua
@@ -4,5 +4,13 @@ return {
     style = "night",
     dim_inactive = true,
     lualine_bold = true,
+    on_highlights = function(highlights, colors)
+      highlights.LineNrAbove = {
+        fg = "#6e6e6e",
+      }
+      highlights.LineNrBelow = {
+        fg = "#6e6e6e",
+      }
+    end,
   },
 }
